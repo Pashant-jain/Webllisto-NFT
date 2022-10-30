015aa25ebdb01490ae3f10f213a09a2275ed860c
prashant jain <prashant.jain@webllito.com>
2022-10-30 17:29:08 +0530

    work on gallery page ui design

--- src/pages/gallery/gallery.js
+++ src/pages/gallery/gallery.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React from "react";
+import { BidCard } from "../../components/bid-card/bid-card";
+import "./gallery.scss";
 
 export const Gallery = () => {
   return (
-    <div>gallery</div>
-  )
-}
+    <div className="gallery_wrp">
+      <div className="container-fluid">
+        <div className="gallery_inner text-center ">
+          <h1>Explore Items</h1>
+          <div className="sort_wrp">
+              <button type="button" >All</button>
+              <button type="button" >Digital Art</button>
+              <button type="button" >All</button>
+            
+          </div>
+          <div className="explore_nfts_wrp" >
+          <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+            <BidCard />
+          </div>
+        
+        </div>
+      </div>
+    </div>
+  );
+};
