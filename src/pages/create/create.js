f80b904a9d1ff2f79181d67855057d79c4bcda7d
prashant <prashant.jain@webllisto.com>
2022-12-08 19:46:57 +0530

    create page changes

--- src/pages/create/create.js
+++ src/pages/create/create.js
@@ -1,79 +1,154 @@
-import React,{useState,useEffect} from "react";
-import metamask_logo from "../../assets/images/metamask_logo.svg";
-import kaikas_logo from "../../assets/images/kaikas_logo.svg";
-import { ethers } from 'ethers'; 
+import React, { useState,useEffect } from "react";
+import "./create.scss";
+import { AllcollectionCategory } from "../../redux/actions/user-create-collection";
+import { useDispatch } from "react-redux";
+import { Dropdown, DropdownButton } from 'react-bootstrap';
 
 export const Create = () => {
-  const [haveMetamask, sethaveMetamask] = useState(true);
-  const [accountAddress, setAccountAddress] = useState('');
-  const [accountBalance, setAccountBalance] = useState('');
-  const [isConnected, setIsConnected] = useState(false);
-  const { ethereum } = window;
-  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const dispatch = useDispatch();
+  const [file, setFile] = useState('');
+  const [allcategory, setAllcategory] = useState();
+  const [selectedCategory, setSelectedCategory] = useState("Select Category");
 
   useEffect(() => {
-    const { ethereum } = window;
-    const checkMetamaskAvailability = async () => {
-      if (!ethereum) {
-        sethaveMetamask(false);
-      }
-      sethaveMetamask(true);
+    const fetchData = async () => {
+      try {
+        const Collaction = await dispatch(AllcollectionCategory());
+        if (Collaction) {
+          setAllcategory(Collaction);
+        }
+      } catch (err) {}
     };
-    checkMetamaskAvailability();
+    fetchData();
   }, []);
 
-
-  const connectWallet = async () => {
-    try {
-      if (!ethereum) {
-        sethaveMetamask(false);
-      }
-      const accounts = await ethereum.request({
-        method: 'eth_requestAccounts',
-      });
-      let balance = await provider.getBalance(accounts[0]);
-      let bal = ethers.utils.formatEther(balance);
-      setAccountAddress(accounts[0]);
-      setAccountBalance(bal);
-      setIsConnected(true);
-    } catch (error) {
-      setIsConnected(false);
-    }
-  };
+  function handleChange(e) {
+    console.log(e.target.files);
+    setFile(URL.createObjectURL(e.target.files[0]));
+  }
   return (
-    <div className="connect_wrp">
-    <div className="container-fluid">
-      <div className="connect_inner text-center ">
-        <h1>Create</h1>
-        <h1>Connect Your Wallet</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a{" "}
-          <br />
-          lacinia dolor, in pretium nunc. Morbi mollis arcu eget.
-        </p>
-        <div className="wallets_wrp">
-          <div   onClick={connectWallet} className="wallet_block text-left ">
-            <figure>
-              <img src={metamask_logo} alt='metamask' />
-            </figure>
-            <h2>Meta Mask</h2>
-            {isConnected ? (<p> {accountAddress.slice(0, 4)}...
-                  {accountAddress.slice(38, 42)}</p>)
-                  :
-                 (<p>Lorem ipsum dolor sit amet consectetur smit.</p>
-            )}
-          </div>
-          <div className="wallet_block">
-            <figure>
-              <img src={kaikas_logo}  alt='kaikas' />
-            </figure>
-            <h2>Kaikas</h2>
-            <p>Lorem ipsum dolor sit amet consectetur smit.</p>
+    <div className="create_wrp">
+      <div className="container-fluid">
+        <div className="create_inner">
+          <h1 className="text-center">Create A New NFT</h1>
+          <div className="create_form_wrp mb-5">
+            <form>
+              <div className="row">
+                <div className="col-lg-5 col-md-12 create_form_lft_wrp">
+                  <label className="form_input_wrp">
+                    <input type="file" className="d-none"  onChange={handleChange} />
+                    <div>
+                      {file === "" ? (
+                        <>
+                        fsgrh
+                        </>
+                      ):(
+                        <>
+                        <img src={file} />
+                        </>
+                      )
+
+                      }
+                    </div>
+                    
+                  </label>
+                </div>
+                <div className="col-lg-7 col-md-12 create_form_right_wrp">
+                  <div className="create_right">
+                    <div className="form-group">
+                      <label htmlFor="title">Title</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="title"
+                        placeholder="Enter item title"
+                      />
+                      <span
+                        style={{
+                          color: "red",
+                          fontSize: 12,
+                          fontWeight: "bold",
+                        }}
+                      >
+                       error
+                      </span>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="description">Description</label>
+                      <textarea
+                        type="email"
+                        className="form-control"
+                        id="description"
+                        placeholder="Type item description"
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="price">Price</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="price"
+                        placeholder="0 ETH"
+                      />
+                        <span
+                        style={{
+                          color: "red",
+                          fontSize: 12,
+                          fontWeight: "bold",
+                        }}
+                      >
+                       error
+                      </span>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="royalties">Royalties *</label>
+                      <input
+                        type="email"
+                        className="form-control"
+                        id="royalties"
+                        placeholder="10"
+                      />
+                    </div>
+                    <div className="form-group category_wrp">
+                      <label htmlFor="royalties">Category *</label>
+                      <Dropdown>  {/* onSelect={changecategory} */}
+                          <Dropdown.Toggle
+                            variant="secondry"
+                           className=""
+                            id="dropdown-basic"
+                          >
+                           {selectedCategory}
+                          </Dropdown.Toggle>
+                          <Dropdown.Menu>
+                            <Dropdown.Item disabled={true} eventKey="">
+                              Select Category
+                            </Dropdown.Item>
+                            {allcategory?.map((val , i) => {
+                                return (
+                                  <>
+                                    <Dropdown.Item
+                                      key={i}
+                                      onClick={() => setSelectedCategory(val.name)}
+                                      eventKey={val._id}
+                                    >
+                                      {val.name}
+                                    </Dropdown.Item>
+                                  </>
+                                );
+                            })}
+                          </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className="submit_wrp" >
+                            <button type="submit"   >Create Item</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
-         <p>Wallet Ballence {accountBalance}</p>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
